5f70e8f859659accd5ba5cb61416f236954c4fee
thatGuyNapster <andrewosei94@gmail.com>
2024-03-19 23:17:12 +0000

    cors fix attempt #8

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
