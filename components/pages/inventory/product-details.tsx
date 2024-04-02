c970c68f8f2b7a7830007c635d39bff6f9a62e91
thatGuyNapster <andrewosei94@gmail.com>
2024-04-02 14:06:46 +0000

    minor UI fixes

--- components/pages/inventory/product-details.tsx
+++ components/pages/inventory/product-details.tsx
@@ -6,12 +6,12 @@ import React from "react";
 const ProductOverview = ({ data }: { data: IProduct }) => {
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex flex-col md:flex-row gap-6 justify-between">
+      <div className="flex flex-col md:flex-row gap-6 items-center justify-between">
         <div className="flex flex-col gap-4 w-full">
           <h3 className={classNames("font-semibold", "dark:text-neutral-50")}>
             Details
           </h3>
-          <div className="flex w-full">
+          <div className="flex w-full items-center">
             <div className="flex flex-col gap-6 w-1/2">
               <p>
                 <span>Name</span>
@@ -29,6 +29,7 @@ const ProductOverview = ({ data }: { data: IProduct }) => {
                 <span>Sale Price</span>
               </p>
             </div>
+
             <div className="flex flex-col gap-6 w-1/3">
               <p className="truncate">{data.name}</p>
               <p className="truncate">{data.category_details?.name ?? "n/a"}</p>
@@ -36,20 +37,21 @@ const ProductOverview = ({ data }: { data: IProduct }) => {
               <p className="truncate">&#8373;{data.cost_price.toFixed(2)}</p>
               <p className="truncate">&#8373;{data.sale_price.toFixed(2)}</p>
             </div>
+
+            <div className="w-[170px] h-[170px] bg-center bg-no-repeat bg-cover rounded-2xl border-2 border-dashed border-neutral-40">
+              <Image
+                width={170}
+                height={170}
+                src={(data.images[0] as string) ?? "/img/logo.png"}
+                alt={`${name} image`}
+                className={`w-full h-full rounded-2xl`}
+                onError={(ev) => {
+                  ev.currentTarget.src = "/img/logo.png";
+                }}
+                priority
+              />
+            </div>
           </div>
-        </div>
-        <div className="w-[170px] bg-white bg-center bg-no-repeat bg-cover rounded-md">
-          <Image
-            width={170}
-            height={170}
-            src={(data.images[0] as string) ?? "/img/logo.png"}
-            alt={`${name} image`}
-            className={`w-full h-full rounded-md`}
-            onError={(ev) => {
-              ev.currentTarget.src = "/img/logo.png";
-            }}
-            priority
-          />
         </div>
       </div>
 
